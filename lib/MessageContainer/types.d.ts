49f8f3082bc999e2434b310b5298eaa64b1a45f7
Sheggsmann <promisesheggs@gmail.com>
2025-03-05 16:09:10 +0100

    pushing the lib as well

---
+++ lib/MessageContainer/types.d.ts
@@ -0,0 +1,54 @@
+import React, { Component, RefObject } from "react";
+import { LayoutChangeEvent, StyleProp, ViewStyle } from "react-native";
+import { LoadEarlierProps } from "../LoadEarlier";
+import { MessageProps } from "../Message";
+import { User, IMessage, Reply } from "../types";
+import { ReanimatedScrollEvent } from "react-native-reanimated/lib/typescript/hook/commonTypes";
+import { AnimateProps } from "react-native-reanimated";
+import { FlashList, FlashListProps } from "@shopify/flash-list";
+export type ListViewProps = {
+    onLayout?: (event: LayoutChangeEvent) => void;
+} & object;
+export type AnimatedList<TMessage> = Component<AnimateProps<FlashListProps<TMessage>>, unknown, unknown> & FlashList<FlashListProps<TMessage>>;
+export interface MessageContainerProps<TMessage extends IMessage = IMessage> {
+    forwardRef?: RefObject<AnimatedList<TMessage>>;
+    messages?: TMessage[];
+    isTyping?: boolean;
+    user?: User;
+    listViewProps?: ListViewProps;
+    inverted?: boolean;
+    loadEarlier?: boolean;
+    alignTop?: boolean;
+    isScrollToBottomEnabled?: boolean;
+    scrollToBottomStyle?: StyleProp<ViewStyle>;
+    invertibleScrollViewProps?: object;
+    extraData?: object;
+    scrollToBottomOffset?: number;
+    renderChatEmpty?(): React.ReactNode;
+    renderFooter?(props: MessageContainerProps<TMessage>): React.ReactNode;
+    renderMessage?(props: MessageProps<TMessage>): React.ReactElement;
+    renderLoadEarlier?(props: LoadEarlierProps): React.ReactNode;
+    renderTypingIndicator?(): React.ReactNode;
+    scrollToBottomComponent?(): React.ReactNode;
+    onLoadEarlier?(): void;
+    onQuickReply?(replies: Reply[]): void;
+    infiniteScroll?: boolean;
+    isLoadingEarlier?: boolean;
+    handleOnScroll?(event: ReanimatedScrollEvent): void;
+}
+export interface State {
+    showScrollBottom: boolean;
+    hasScrolled: boolean;
+}
+interface ViewLayout {
+    x: number;
+    y: number;
+    width: number;
+    height: number;
+}
+export type DaysPositions = {
+    [key: string]: ViewLayout & {
+        createdAt: number;
+    };
+};
+export {};
